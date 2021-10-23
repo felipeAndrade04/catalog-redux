8fe90a37c832c8d9047bc751393446dfdb2555d0
Felipe Andrade <flpandrade2011@gmail.com>
2021-10-23 11:04:14 -0300

    ✨ feat: load products from api

--- src/components/Catalog/index.tsx
+++ src/components/Catalog/index.tsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react";
+
+import api from "../../services/api";
+import { IProduct } from "../../store/modules/cart/types";
+
 function Catalog() {
+  const [catalog, setCatalog] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    api.get('products').then(response => {
+      setCatalog(response.data as IProduct[]);
+    })
+  }, []);
+
+  console.log(catalog);
+
   return (
-    <h1>Catalog</h1>
+    <main>
+      <h1>Catalog</h1>
+
+      {catalog.map(product => (
+        <article key={product.id}>
+          <strong>{product.title}</strong> {" - "}
+          <span>{product.price}</span> {"  "}
+
+          <button type="button">Comprar</button>
+        </article>
+      ))}
+    </main>
   );
 }
 
