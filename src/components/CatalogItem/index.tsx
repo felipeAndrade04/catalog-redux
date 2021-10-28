5f9925cbdf0979648aca48918e5459c7f0de0c57
Felipe Andrade <flpandrade2011@gmail.com>
2021-10-28 08:34:11 -0300

    💄 chore: adjusts CatalogItem

--- src/components/CatalogItem/index.tsx
+++ src/components/CatalogItem/index.tsx
@@ -1,5 +1,6 @@
 import { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Flex, Button, Text } from "@chakra-ui/react";
 
 import { IProduct } from "../../store/modules/cart/types";
 import { addProductToCartRequest } from "../../store/modules/cart/actions";
@@ -21,19 +22,21 @@ function CatalogItem({ product }: CatalogItemProps) {
   }, [dispatch]);
 
   return (
-    <article>
-      <strong>{product.title}</strong> {" - "}
-      <span>{product.price}</span> {"  "}
+    <Flex p={4} justify="space-between" align="center" bg="gray.100" borderRadius="base" border="1px" borderColor="gray.400" boxShadow="md">
+      <Text fontSize="1em" fontWeight="600">{product.title}</Text>
+      <Text fontSize="1em">R$ {product.price}</Text>
 
-      <button
-        type="button"
-        onClick={() => handleAddProductToCart(product)}
-      >
-        Comprar
-      </button>
-
-      {hasFailedStockCheck && <span style={{ color: 'red' }}>Falta de estoque</span>}
-    </article>
+      {hasFailedStockCheck
+        ? <Text fontSize="1em" color="red.400">Sem estoque</Text>
+        : <Button
+          size="sm"
+          colorScheme="purple"
+          onClick={() => handleAddProductToCart(product)}
+        >
+          Comprar
+        </Button>
+      }
+    </Flex>
   );
 }
 
